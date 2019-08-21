4ab5e64c6769f58bb2831ee1bdd94b5ddf77ad59
Hristian Ilioski <djkiko15@gmail.com>
2019-08-21 22:42:53 +0200

    User table seeder added.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(UsersTableSeeder::class);
         $this->call(ActorsTableSeeder::class);
         $this->call(MoviesTableSeeder::class);
         $this->call(AssignMoviesActorsSeeder::class);
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class, 10)->create();
+        factory(User::class, 1)->create([
+            'name' => 'Example User',
+            'email' => 'email@example.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
